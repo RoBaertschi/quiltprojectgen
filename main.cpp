a95734d020c4da59ad3e288f000c949cdc7120e6
RoBaertschi <rtmbaertschi007@gmail.com>
2022-05-13 17:59:30 +0200

    First Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-
+#include <iostream>
+#include <filesystem>
 
 int main(int argc, char *argv[])
 {
-	
+    std::cout << std::filesystem::current_path();
+    std::system("git clone https://github.com/QuiltMC/quilt-template-mod testquilt");
+    std::filesystem::remove_all("testquilt/.git");
 }
