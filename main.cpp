71a048acaf4f0831e2ef25dd176c069128ba3c25
RoBaertschi <rtmbaertschi007@gmail.com>
2022-05-15 14:36:24 +0200

    Got testing running.

--- main.cpp
+++ main.cpp
@@ -7,22 +7,29 @@ namespace qpg
     void cloneRepo(std::string nameOfDir) {
         std::string cloneString{"git clone https://github.com/QuiltMC/quilt-template-mod " + nameOfDir};
         int returnValue{std::system(cloneString.c_str())};
-        std::filesystem::remove_all(nameOfDir + "/.git");
-        if(returnValue == 128) {
-            throw std::invalid_argument("Please chose an other folder or make sure that nothing is in the folder.");
+        if(returnValue != 0) {
+            throw std::invalid_argument("Error ocoured while running git.");
         }
+        std::filesystem::remove_all(nameOfDir + "/.git");
+        
     }
 }
 
 int main(int argc, char *argv[])
 {
-    if(argc >= 2) {
-        std::string path{argv[1]};
-        qpg::cloneRepo(path);
+    try {
+        if(argc >= 2) {
+            std::string path{argv[1]};
+            qpg::cloneRepo(path);
+        }
+        else
+        {
+            throw std::invalid_argument(std::string{"There are not enough Arguments for this Command\n"} + 
+            std::string{"quiltprojectgen <PATH>"});
+        }
     }
-    else
-    {
-        throw std::invalid_argument(std::string{"There are not enough Arguments for this Command\n"} + 
-        std::string{"quiltprojectgen <PATH>"});
+    catch (const std::invalid_argument& e) {
+        std::cerr << e.what();
+        return 1;
     }
 }
