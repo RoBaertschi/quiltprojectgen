c3bfd8a7254e7ed6f44285ebd9ccaab5fce5d275
RoBaertschi <rtmbaertschi007@gmail.com>
2022-05-18 17:40:44 +0200

    Added -r Argument

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,27 @@ int main(int argc, char *argv[])
 {
     try {
         if(argc >= 2) {
+            if (argc >= 3 && std::string{"-r"}.compare(argv[1]))
+            {
+                try
+                {
+                    std::filesystem::remove_all(argv[1]);
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << "There is no Directory called " + std::string{argv[1]} << std::endl;
+                    std::cerr << e.what() << '\n';
+                }
+                
+            }
+            
             std::string path{argv[1]};
             qpg::cloneRepo(path);
         }
         else
         {
             throw std::invalid_argument(std::string{"There are not enough Arguments for this Command\n"} + 
-            std::string{"quiltprojectgen <PATH>\n"});
+            std::string{"quiltprojectgen <PATH> [-r]\n"});
         }
     }
     catch (const std::invalid_argument& e) {
